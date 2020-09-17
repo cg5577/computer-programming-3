c962103477ff197697a6ed48d7d2dd7ef576792c
anjali678 <nayananjali631@gmail.com>
2020-09-18 04:01:51 +0530

    adding java file1

---
+++ CscExample.java
@@ -0,0 +1,16 @@
+clas CscExample
+{
+	int questionNo;
+	String category;
+	String text;
+	
+	public void setQuestionNo()
+	{}
+	
+	public void setCategory()
+	{}
+	
+	public void setText()
+	{}
+	
+}
